1d6cb7e083ef2f343fc97d66d0841f204d1b4a9e
Zachary Proser <zackproser@gmail.com>
2022-01-23 17:44:11 -0500

    Implement configuration of blocked hosts

--- cmd/root.go
+++ cmd/root.go
@@ -19,11 +19,15 @@ import (
 	"fmt"
 	"os"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	logLevel string
+	cfgFile  string
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -50,7 +54,8 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.procrastiproxy.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", ".procrastiproxy.yaml", "config file (default is $HOME/.procrastiproxy.yaml)")
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "debug", "Log level to set. Valid options: {INFO|DEBUG|WARN|TRACE|FATAL}")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -79,4 +84,9 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
+	level, err := log.ParseLevel(viper.GetString("log-level"))
+	if err != nil {
+		level = log.DebugLevel
+	}
+	log.SetLevel(level)
 }
--- cmd/server.go
+++ cmd/server.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
@@ -10,14 +11,43 @@ import (
 	"github.com/spf13/viper"
 )
 
+func hostIsBlocked(host string) bool {
+	host = strings.ToLower(strings.TrimSpace(host))
+	fmt.Printf("hostIsBlocked examining: %s\n", host)
+	fmt.Printf("hostIsBlocked: %+v\n", viper.GetStringSlice("blocked_hosts"))
+	for _, blockedHost := range viper.GetStringSlice("blocked_hosts") {
+		fmt.Printf("blockedHost: %s and host: %s\n", blockedHost, host)
+		if blockedHost == host {
+			return true
+		}
+	}
+	return false
+}
+
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	log.WithFields(logrus.Fields{
+		"Host": r.URL.Host,
+		"Path": r.URL.Path,
+	}).Debug("Proxy handler received request")
+
+	log.WithFields(logrus.Fields{
+		"blocked sites": viper.Get("blocked_hosts"),
+	}).Debug("Blocked site hosts")
+
+	if hostIsBlocked(r.URL.Host) {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Forbidden"))
+	} else {
+		w.Write([]byte("Allowing: " + r.URL.Host))
+	}
+}
+
 func RunServer(cmd *cobra.Command, args []string) {
 	log.WithFields(logrus.Fields{
-		"Port": viper.Get("port"),
+		"Port": args[0],
 	}).Info("Proxy listening...")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Pong")
-	})
+	http.HandleFunc("/", proxyHandler)
 
-	log.Fatal(http.ListenAndServe(":"+viper.GetString("port"), nil))
+	log.Fatal(http.ListenAndServe(":"+args[0], nil))
 }
