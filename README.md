742077cbc2b25bb3e138a03f16c909d828b6bf5b
Zachary Proser <zackproser@gmail.com>
2022-01-23 21:44:47 -0500

    Improve README. Relocate handlers

--- README.md
+++ README.md
@@ -2,7 +2,28 @@
 
 Procrastiproxy is a proxy designed to help you focus during the day by blocking distracting websites.
 
+It implements an in-memory, mutable list for tracking hosts that should be blocked by the proxy. This in-memory list allows for fast (`O(1)` or "constant time") look-ups.
+
 # Running locally
 
 `go build`
-`./procrastiproxy --config cmd/.procrastiproxy.yaml`
+
+`./procrastiproxy 8001 --config .procrastiproxy.yaml`
+
+# Admin control
+
+Make a request to the `<server-root>/admin/` path, passing either `block` or `unblock` followed by a host, like so:
+
+### Add a new host to the block list
+
+`curl http://localhost:8001/admin/block/reddit.com`
+
+### Remove a host from the block list
+
+`curl http://localhost:8001/admin/unblock/reddit.com`
+
+# Running tests
+
+Procrastiproxy comes complete with tests to verify its functionality.
+
+`go test -v ./...`
--- cmd/server.go
+++ cmd/server.go
@@ -1,60 +1,18 @@
 package cmd
 
 import (
-	"io/ioutil"
 	"net/http"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 func hostIsBlocked(host string) bool {
 	host = strings.ToLower(strings.TrimSpace(host))
 	blockList := GetList()
 	return blockList.Contains(host)
-}
-
-func proxyHandler(w http.ResponseWriter, r *http.Request) {
-	log.WithFields(logrus.Fields{
-		"Host": r.URL.Host,
-		"Path": r.URL.Path,
-	}).Debug("Proxy handler received request")
-
-	log.WithFields(logrus.Fields{
-		"blocked sites": viper.Get("blocked_hosts"),
-	}).Debug("Blocked site hosts")
-
-	if hostIsBlocked(r.URL.Host) {
-		w.WriteHeader(http.StatusForbidden)
-		w.Write([]byte("Forbidden"))
-	} else {
-		// Perform the supplied request and return the response to caller
-		res, err := http.Get(r.URL.String())
-		if err != nil {
-			log.Fatal(err)
-		}
-		body, err := ioutil.ReadAll(res.Body)
-		// Return the request to caller by performing a very rudimentary clone of it, here
-		w.WriteHeader(res.StatusCode)
-		w.Write(body)
-	}
-}
-
-func adminHandler(w http.ResponseWriter, r *http.Request) {
-	log.WithFields(logrus.Fields{
-		"Path": r.URL.Path,
-	}).Debug("Admin handler received request")
-	pathElem := strings.Split(r.URL.Path, "/")
-	log.Printf("pathElem: %+v pathElem[3]: %+v\n", pathElem, pathElem[2])
-	if len(pathElem) < 1 {
-		log.Printf("Received malformed request path: %s\n", r.URL.Path)
-	}
-	if pathElem[2] == "block" || pathElem[2] == "unblock" {
-		log.Printf("Received valid command: %s\n", pathElem[2])
-	}
 }
 
 func RunServer(cmd *cobra.Command, args []string) {
