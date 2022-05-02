46b43f442d3bd9d444c8a2a099edef742e133ab6
Zachary Proser <zackproser@gmail.com>
2022-05-02 09:37:59 -0400

    Clean up installation assets once they're no longer needed

--- install.sh
+++ install.sh
@@ -12,6 +12,13 @@ then
   arch="x86_64"
 fi
 
+# Convenience method for deleting the downloaded tarball and expanded folder after installation
+cleanupInstallationDir () {
+  echo "Cleaning up installation downloads..."
+  rm -rf $1 $2 
+}
+
+# Fetch the latest release of procrastiproxy that is compatible with the machine this script is being executed on 
 get_release_url () {
 jq_cmd=".assets[] | select(.name | endswith(\"${platform}-${arch}.tar.gz\")).browser_download_url"
 # Find the latest binary release URL for this platform
@@ -21,18 +28,23 @@ echo $url
 
 target_url="$(get_release_url)"
 curl -LO $target_url
-#Rename and copy to your procrastiproxy folder
+# Rename and copy to your procrastiproxy folder
 filename=$(basename $target_url)
+# Unpack the tarball to your pwd
 tar xvzf "${filename}"
 binaryname="procrastiproxy"
 unzippeddir="${filename/.tar.gz/""}"
 fullpath="${unzippeddir}/${binaryname}"
+# Make binary executable
 chmod +x "${fullpath}"
 
+# Move the binary into the target directory
 PROCRASTIPROXY_DIR=/usr/local/bin/procrastiproxy
 sudo mv "${fullpath}" "${PROCRASTIPROXY_DIR}"
 if [[ $? -eq 0 ]]; then
   echo "Successfully installed $binary_name at $PROCRASTIPROXY_DIR"
+  # Delete the downloaded tarball and the expanded folder now that installation is complete
+  cleanupInstallationDir "$unzippeddir" "$filename"
 else
   echo "ERROR: could not install $binary_name at $PROCRASTIPROXY_DIR"
 fi
